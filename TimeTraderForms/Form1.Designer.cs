7c5b7baded69b2e832ae15d3b1e9f86a1305cf42
AQAD Abdel <48525287+aqadabdel@users.noreply.github.com>
2019-04-18 01:24:18 +0200

    Add files via upload

---
+++ TimeTraderForms/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TimeTraderForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblNY = new System.Windows.Forms.Label();
+            this.grpNY = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblLondon = new System.Windows.Forms.Label();
+            this.pbLondon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.grpNY.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLondon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(27, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 205);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // lblNY
+            // 
+            this.lblNY.AutoSize = true;
+            this.lblNY.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNY.Location = new System.Drawing.Point(73, 233);
+            this.lblNY.Name = "lblNY";
+            this.lblNY.Size = new System.Drawing.Size(95, 16);
+            this.lblNY.TabIndex = 1;
+            this.lblNY.Text = "LOCAL TIME";
+            this.lblNY.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // grpNY
+            // 
+            this.grpNY.Controls.Add(this.pictureBox1);
+            this.grpNY.Controls.Add(this.lblNY);
+            this.grpNY.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpNY.Location = new System.Drawing.Point(12, 12);
+            this.grpNY.Name = "grpNY";
+            this.grpNY.Size = new System.Drawing.Size(248, 262);
+            this.grpNY.TabIndex = 3;
+            this.grpNY.TabStop = false;
+            this.grpNY.Text = "New York TIME";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblLondon);
+            this.groupBox2.Controls.Add(this.pbLondon);
+            this.groupBox2.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(266, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(248, 262);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "London TIME";
+            // 
+            // lblLondon
+            // 
+            this.lblLondon.AutoSize = true;
+            this.lblLondon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLondon.Location = new System.Drawing.Point(73, 233);
+            this.lblLondon.Name = "lblLondon";
+            this.lblLondon.Size = new System.Drawing.Size(95, 16);
+            this.lblLondon.TabIndex = 1;
+            this.lblLondon.Text = "LOCAL TIME";
+            this.lblLondon.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbLondon
+            // 
+            this.pbLondon.Location = new System.Drawing.Point(23, 22);
+            this.pbLondon.Name = "pbLondon";
+            this.pbLondon.Size = new System.Drawing.Size(205, 205);
+            this.pbLondon.TabIndex = 0;
+            this.pbLondon.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 279);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.grpNY);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.grpNY.ResumeLayout(false);
+            this.grpNY.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLondon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblNY;
+        private System.Windows.Forms.GroupBox grpNY;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblLondon;
+        private System.Windows.Forms.PictureBox pbLondon;
+    }
+}
+
---
+++ TimeTraderForms/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace TimeTraderForms
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+
+        int WIDTH = 200, HEIGHT = 200, secHAND = 97, minHAND = 80, hrHAND = 60;
+        
+        int cx, cy;
+
+
+        Bitmap bmp;
+        Graphics g;
+        
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cx = WIDTH / 2;
+            cy = HEIGHT / 2;
+
+            this.BackColor = Color.White;
+            bmp = new Bitmap(WIDTH + 1, HEIGHT + 1);
+
+            
+            //Timer 
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.timeTick);
+            t.Start();
+        }
+
+        private void GrpParis_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timeTick(object sender, EventArgs e)
+        {
+
+            DateTime NyTime = DateTime.Now;
+                    
+            Console.WriteLine("TIME " + NyTime.ToString());
+           
+
+            DrawClock(NyTime,  pictureBox1, lblNY);
+            DateTime ParisTime = DateTime.Now;
+            ParisTime = ParisTime.AddHours(3);
+            Console.WriteLine("TIME " + ParisTime.AddHours(2.0).ToLongTimeString());
+            DrawClock(ParisTime,  pbLondon, lblLondon);
+          //  DrawClock(NyTime, bmp, pictureBox3);
+
+
+        }
+       
+        private void DrawClock(DateTime dt, PictureBox pb, Label lbl)
+        {
+
+
+
+            g = Graphics.FromImage(bmp);
+            //get Time
+            int ss = dt.Second;
+            int mm = dt.Minute;
+            int hh = dt.Hour;
+            Console.WriteLine("{0} {1} {2}", hh, mm, ss);
+            int[] handCoord = new int[2];
+
+            //g clear
+            g.Clear(Color.White);
+            //Draw circle
+            g.DrawEllipse(new Pen(Color.Black, 2f), 0, 0, WIDTH, HEIGHT);
+            g.FillEllipse(new SolidBrush(Color.Lavender), 0, 0, WIDTH, HEIGHT);
+            g.DrawString("12", new Font("Verdana", 14), Brushes.DarkSlateBlue, new PointF(WIDTH / 2 - 10, 4));
+            g.DrawString("3", new Font("Verdana", 14), Brushes.DarkSlateBlue, new PointF(WIDTH - 20, HEIGHT / 2 - 6));
+            g.DrawString("6", new Font("Verdana", 14), Brushes.DarkSlateBlue, new PointF(5, HEIGHT / 2 - 6));
+            g.DrawString("9", new Font("Verdana", 14), Brushes.DarkSlateBlue, new PointF(WIDTH / 2 - 5, HEIGHT - 25));
+            //g.DrawRectangle(new Pen(Color.Black), cx-5, cy-5, 10, 10);
+            // g.DrawEllipse(new Pen(Color.Blue, 5), cx - 3, cy - 3, 3, 3);
+            g.FillEllipse(new SolidBrush(Color.Lavender), cx - 3, cy - 3, 3, 3);
+
+            handCoord = msCoord(ss, secHAND);
+            g.DrawLine(new Pen(Color.Red, 1f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            handCoord = msCoord(mm, minHAND);
+            g.DrawLine(new Pen(Color.Orchid, 2f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            
+
+            handCoord = hrCoord(hh % 12, mm, hrHAND);
+            g.DrawLine(new Pen(Color.CornflowerBlue, 4f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            pb.Image = bmp;
+
+            lbl.Text = "Analog Clock - " + hh + ":" + mm + ":" + ss;
+            
+            g.Dispose();
+           // bmp = null;
+            //bmp.Dispose();
+
+
+        }
+
+        private int[] msCoord ( int val, int hlen)
+        {
+            int[] coord = new int[2];
+            val *= 6;
+
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cx - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cx - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+
+        private int[] hrCoord(int hval, int mval, int hlen)
+        {
+            int[] coord = new int[2];
+            Console.WriteLine("HVAL: " + hval);
+            //each hour makes 30 degree
+            //each min makes 0.5 degree
+            int val = (int)((hval * 30) + (mval * 0.5));
+           
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            Console.WriteLine("HANDCOORD {0} {1}", coord[0], coord[1]);
+            return coord;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
